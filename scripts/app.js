8bb996b281706f59f75fbb7f48da14284a53b7b6
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-11-16 10:15:13 +0800

    Able to change location and persist to LS.

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
-const weather = new Weather("Tanza", "Cavite");
+const storage = new Storage();
+const weatherLocation = storage.getLocationData();
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 const ui = new UI();
-// weather.changeLocation()
 
 const getWeather = () => {
   weather
@@ -15,3 +16,14 @@ const getWeather = () => {
 };
 
 document.addEventListener("DOMContentLoaded", getWeather);
+document.querySelector("#w-change-btn").addEventListener("click", (e) => {
+  const city = document.querySelector("#city").value;
+  const state = document.querySelector("#state").value;
+
+  weather.changeLocation(city, state);
+  storage.setLocationData(city, state);
+
+  getWeather();
+
+  $("#locModal").modal("hide");
+});
